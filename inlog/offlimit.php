4d9c1d455fc911d36d9cb3ca1b71d48a3f4f0c08
Kevin <kevinhiemstra@gmail.com>
2022-09-05 10:57:18 +0200

    change

--- inlog/offlimit.php
+++ inlog/offlimit.php
@@ -1,27 +1,32 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION["role"] !== "admin"){
-    header("location: offlimit.php");
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
     exit;
 }
+
+require_once "config.php";
+
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Nope</title>
     <style>
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi Admin, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+<h1>    
+    Sorry you don't have the rights to see this content.
+</h1>
     <p>
-        <a href="logout.php" >Sign Out of Your Account</a>
+        <a href="welcome.php" >Ok</a>
     </p>
 </body>
 </html>
